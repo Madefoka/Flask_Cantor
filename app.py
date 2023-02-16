6eb7afaac843d3ed9fc8a83c6814e8a8fbb1487b
Madefoka <raddas@gazeta.pl>
2023-02-16 19:29:47 +0100

    Dodanie panelu edycji użytkownika.

--- app.py
+++ app.py
@@ -295,7 +295,33 @@ def users_status_change(action, user_name):
 
 @app.route("/edit_user/<user_name>", methods=["GET", "POST"])
 def edit_user(user_name):
-    return "not implemented yet"
+
+    db = get_db()
+    cur = db.execute(
+        "select name, email from users where name = ?", [user_name])
+    user = cur.fetchone()
+    message = None
+
+    if request.method == "GET":
+        return render_template("edit_user.html", active_menu="users", user=user)
+    else:
+        new_email = "" if "email" not in request.form else request.form["email"]
+        new_password = "" if "user_pass" not in request.form else request.form["user_pass"]
+
+        if new_email != user["email"]:
+            sql_statement = "Update users set email = ? where name = ?"
+            db.execute(sql_statement, [new_email, user_name])
+            db.commit()
+            flash('Email was changed.')
+
+        if new_password != "":
+            user_pass = UserPass(user_name, new_password)
+            sql_statement = "update users set password = ? where name = ?"
+            db.execute(sql_statement, [user_pass.hash_password(), user_name])
+            db.commit()
+            flash("Password was changed.")
+
+        return redirect(url_for("users"))
 
 
 @app.route("/user_delete/<user_name>")
